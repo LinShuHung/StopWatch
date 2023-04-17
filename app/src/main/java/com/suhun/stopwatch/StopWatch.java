9b75f623f2b52650c9578a3d831b9d1b3238227e
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-04-17 13:23:13 +0800

    Add timer to add counter when press start

--- app/src/main/java/com/suhun/stopwatch/StopWatch.java
+++ app/src/main/java/com/suhun/stopwatch/StopWatch.java
@@ -1,22 +1,37 @@
 package com.suhun.stopwatch;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.suhun.stopwatch.databinding.ActivityMainBinding;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class StopWatch {
     public String tag = StopWatch.class.getSimpleName();
     private ActivityMainBinding binding;
     private Context context;
     private boolean isStart;
+    private int counter;
     private Timer timer;
+    private MyTask myTask;
+
+    private class MyTask extends TimerTask{
+        @Override
+        public void run() {
+            counter++;
+            Log.d(tag, "Counter:" + counter);
+            binding.clock.setText(""+ counter);
+
+        }
+    }
 
     public StopWatch(Context context, ActivityMainBinding binding){
         this.context = context;
         this.binding = binding;
         this.timer = new Timer();
+        this.counter = 0;
     }
 
     public void doLeft(){
@@ -33,12 +48,18 @@ public class StopWatch {
     }
 
     private void doStart() {
+        myTask = new MyTask();
+        timer.schedule(myTask, 10, 10);
         binding.leftBtn.setText("Lap");
         binding.rightBtn.setText("Stop");
     }
 
     private void doStop(){
-        binding.leftBtn.setText("Reset");
-        binding.rightBtn.setText("Start");
+        if(myTask != null){
+            myTask.cancel();
+            myTask = null;
+            binding.leftBtn.setText("Reset");
+            binding.rightBtn.setText("Start");
+        }
     }
 }
