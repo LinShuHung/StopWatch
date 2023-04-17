3401b1c45fff5e80b0b45df475052f9dde031846
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-04-17 13:27:41 +0800

    Add class UIHandler to handle setText(binding.color.setText(counter); To prevent some device assert automatic;

--- app/src/main/java/com/suhun/stopwatch/StopWatch.java
+++ app/src/main/java/com/suhun/stopwatch/StopWatch.java
@@ -1,7 +1,11 @@
 package com.suhun.stopwatch;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import com.suhun.stopwatch.databinding.ActivityMainBinding;
 
@@ -16,14 +20,22 @@ public class StopWatch {
     private int counter;
     private Timer timer;
     private MyTask myTask;
+    private UIHandler uiHandler;
 
     private class MyTask extends TimerTask{
         @Override
         public void run() {
             counter++;
             Log.d(tag, "Counter:" + counter);
-            binding.clock.setText(""+ counter);
+            uiHandler.sendEmptyMessage(0);
+        }
+    }
 
+    private class UIHandler extends Handler{
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            binding.clock.setText(""+ counter);
         }
     }
 
@@ -31,6 +43,7 @@ public class StopWatch {
         this.context = context;
         this.binding = binding;
         this.timer = new Timer();
+        this.uiHandler = new UIHandler();
         this.counter = 0;
     }
 
