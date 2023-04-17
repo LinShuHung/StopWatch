2f92504a8d44964b83d78b1a2c42a60746b71c1e
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-04-17 10:54:56 +0800

    Add layout for StopWatch

--- app/src/main/java/com/suhun/stopwatch/MainActivity.java
+++ app/src/main/java/com/suhun/stopwatch/MainActivity.java
@@ -3,6 +3,7 @@ package com.suhun.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.suhun.stopwatch.databinding.ActivityMainBinding;
 
@@ -15,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    public void leftFButton(View view){
+
+    }
+
+    public void rightButton(View view){
+
+    }
+
+    public void exitButton(View view){
+
     }
 }
